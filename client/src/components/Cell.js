5925f3e5b9b00d206e3223101339d6a80828ee6d
Jorge Díaz <jorgediazsamaniego@gmail.com>
2019-07-17 15:19:42 -0400

    Ability to 'flag' a cell with a red flag

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react'
 
 import bomb from '../images/bomb.png'
+import flag from '../images/flag.png'
 
-export default class Cell extends Component {
+class Cell extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      status: this.props.status
+    }
+  }
   renderIcon = (status, value, gameStatus) => {
-    if (status === 'hidden' && gameStatus) return ' '
+    if (status === 'hidden' && gameStatus === 'playing') return ' '
+    if (status === 'flag') {
+      return <img src={flag} alt="flag" width="15" />
+    }
     if (value === -1) {
       return <img src={bomb} alt="bomb" width="15" />
+    }
+    if (value === 0) {
+      return ' '
     }
     return value
   }
 
+  handleRightClick = (e) => {
+    this.setState({
+      status: this.state.status === 'flag' ? 'hidden' : 'flag'
+    })
+    e.preventDefault()
+  }
+
   render() {
-    const { handleCellClick, x, y, status, value, gameStatus } = this.props
+    const { handleCellClick, x, y, value, gameStatus } = this.props
+
+    let { status } = this.state
+    
+    if (this.props.status === 'revealed') {
+      status = this.props.status
+    }
+
     return (
-      <div className="cell" onClick={() => {if (status === "hidden") {handleCellClick(x, y)}}}>
+      <div
+        className={`cell ${status === 'revealed' ? `cell-revealed` : `` }`}
+        onClick={() => {if (status === "hidden") {handleCellClick(x, y)}}}
+        onContextMenu={(e) => this.handleRightClick(e)}
+      >
         {this.renderIcon(status, value, gameStatus)}
       </div>
     )
   }
 }
+
+export default Cell
